cef9fcaf2de01650280c45fb9f927fa3df84c1c8
Maitri Vira <maitrivira1@gmail.com>
2021-11-15 22:15:44 +0700

    searchview

---
+++ dicodingSubmission3SwiftUI/Core/Domain/UseCase/SearchInteractor.swift
@@ -0,0 +1,29 @@
+//
+//  SearchInteractor.swift
+//  dicodingSubmission3SwiftUI
+//
+//  Created by Maitri Vira on 15/11/21.
+//
+
+import SwiftUI
+import RxSwift
+
+protocol SearchUseCase{
+    
+    func getSearch(by title: String) -> Observable<[RestaurantModel]>
+    
+}
+
+class SearchInteractor: SearchUseCase {
+    
+    private let repository: RestoRepositoryProtocol
+    
+    required init(repository: RestoRepositoryProtocol){
+        self.repository = repository
+    }
+    
+    func getSearch(by title: String) -> Observable<[RestaurantModel]>{
+        return repository.getSearchRestaurant(by: title)
+    }
+    
+}
--- dicodingSubmission3SwiftUI/Module/Search/View/Searchview.swift
+++ dicodingSubmission3SwiftUI/Module/Search/View/Searchview.swift
@@ -2,17 +2,49 @@
 //  SearchView.swift
 //  dicodingSubmission3SwiftUI
 //
-//  Created by Maitri Vira on 14/11/21.
+//  Created by Maitri Vira on 15/11/21.
 //
 
 import SwiftUI
 
 struct SearchView: View {
+    
+    @State var isSearching: Bool = false
+    @ObservedObject var presenter: SearchPresenter
+    
     var body: some View {
         NavigationView {
             VStack {
+                SearchBar(
+                  text: $presenter.title,
+                  onSearchButtonClicked: presenter.searchResto
+                )
                 
-                Text("search")
+                ZStack {
+                  if presenter.loadingState {
+                    loadingIndicator
+                  } else if presenter.title.isEmpty {
+                    emptyTitle
+                  } else if presenter.restaurants.isEmpty {
+                    emptyMeals
+                  } else if !presenter.errorMessage.isEmpty {
+                    errorIndicator
+                  } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                      ForEach(
+                        self.presenter.restaurants,
+                        id: \.id
+                      ) { restaurant in
+                        ZStack {
+                          self.presenter.linkBuilder(for: restaurant, detailRestaurant: dummyData) {
+                              RestoList(restaurant: restaurant)
+                                  .aspectRatio(2/3, contentMode: .fit)
+                          }
+                        }.padding(8)
+                      }
+                    }
+                  }
+                }
                 
             }
             .navigationBarTitle("Search", displayMode: .inline)
@@ -20,8 +52,33 @@ struct SearchView: View {
     }
 }
 
-struct Searchview_Previews: PreviewProvider {
-    static var previews: some View {
-        SearchView()
+extension SearchView {
+  
+  var loadingIndicator: some View {
+    VStack {
+      Text("Loading...")
+      ActivityIndicator()
     }
+  }
+  
+  var errorIndicator: some View {
+    CustomEmptyView(
+      image: "assetSearchNotFound",
+      title: presenter.errorMessage
+    ).offset(y: 80)
+  }
+  
+  var emptyTitle: some View {
+    CustomEmptyView(
+      image: "assetSearchMeal",
+      title: "Come on, find your favorite food!"
+    ).offset(y: 50)
+  }
+  var emptyMeals: some View {
+    CustomEmptyView(
+      image: "assetSearchNotFound",
+      title: "Data not found"
+    ).offset(y: 80)
+  }
+  
 }
